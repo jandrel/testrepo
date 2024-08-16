c8d6b702d8825e1819c50901659243f794530630
Jen Andrella <31245051+jandrel@users.noreply.github.com>
2024-08-15 21:05:11 -0400

    Update storymap.js

--- storymap/scripts/storymap.js
+++ storymap/scripts/storymap.js
@@ -450,12 +450,12 @@ $(window).on('load', function() {
     $('div#contents').animate({scrollTop: '1px'});
 
     // On first load, check hash and if it contains an number, scroll down
-    if (parseInt(location.hash.substr(1))) {
-      var containerId = parseInt( location.hash.substr(1) ) - 1;
-      $('#contents').animate({
-        scrollTop: $('#container' + containerId).offset().top
-      }, 2000);
-    }
+    //if (parseInt(location.hash.substr(1))) {
+    //  var containerId = parseInt( location.hash.substr(1) ) - 1;
+    //  $('#contents').animate({
+    //    scrollTop: $('#container' + containerId).offset().top
+   //   }, 2000);
+   // }
 
     // Add Google Analytics if the ID exists
     var ga = getSetting('_googleAnalytics');
